701504684748fa13a6f5094fea6710f2012315fd
FelixSelvik <felixsandrup@gmail.com>
2023-05-16 00:09:52 +0200

    basic homepage structure

--- project_env/chat_app/home/urls.py
+++ project_env/chat_app/home/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
   path('', views.home, name="home"),
   path('', views.login, name="login"),
